c066aeb66b5b750b87c660cddc9b90ff74ddef11
Asnur Ramdani <asnurramdhani12@gmail.com>
2022-03-29 14:45:54 +0700

    Add Persil Layer

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 mapboxgl.accessToken =
-  "pk.eyJ1IjoibWVudGhvZWxzciIsImEiOiJja3M0MDZiMHMwZW83MnVwaDZ6Z2NhY2JxIn0.vQFxEZsM7Vvr-PX3FMOGiQ";
+  "pk.eyJ1IjoicGVyYXJlIiwiYSI6ImNrbWQzcGc1cjJodTEybnAxYXR4NXkwdGIifQ.OZdSBzbYLGl-5upcKpb1dA";
 const map = new mapboxgl.Map({
   container: "map",
-  style: "mapbox://styles/menthoelsr/ckp6i54ay22u818lrq15ffcnr",
+  style: "mapbox://styles/perare/ckpntmyyg0tez17o6yzz9b0om",
   center: [107.9069417, -6.8122635],
-  zoom: 10,
+  zoom: 15,
+  // worldCopyJump: true,
 });
 const draw = new MapboxDraw({
   displayControlsDefault: false,
@@ -34,6 +35,88 @@ map.on("load", () => {
       visibility: "none",
     },
   });
+
+  //Layer Persil
+  map.addSource("persil", {
+    type: "vector",
+    url: "mapbox://perare.plot",
+  }),
+    map.addLayer({
+      id: "persil-border",
+      type: "line",
+      source: "persil",
+      "source-layer": "test",
+      layout: {
+        "line-join": "round",
+        "line-cap": "round",
+      },
+      paint: {
+        "line-width": 1,
+        "line-opacity": 1,
+        "line-color": [
+          "match",
+          ["get", "ot"],
+          0,
+          "#8D99A7",
+          1,
+          "#FFCA00",
+          2,
+          "#12A4ED",
+          3,
+          "#7ED320",
+          4,
+          "#FF8500",
+          5,
+          "#7854F6",
+          6,
+          "#58C0F3",
+          7,
+          "#F5325C",
+          "#ccc",
+        ],
+      },
+      layout: {
+        visibility: "none",
+      },
+    }),
+    map.addLayer({
+      id: "persil-fill",
+      type: "fill",
+      source: "persil",
+      "source-layer": "test",
+      paint: {
+        "fill-opacity": [
+          "case",
+          ["boolean", ["feature-state", "hover"], !1],
+          1,
+          0.5,
+        ],
+        "fill-color": [
+          "match",
+          ["get", "ot"],
+          0,
+          "#8D99A7",
+          1,
+          "#FFCA00",
+          2,
+          "#12A4ED",
+          3,
+          "#7ED320",
+          4,
+          "#FF8500",
+          5,
+          "#7854F6",
+          6,
+          "#58C0F3",
+          7,
+          "#F5325C",
+          "#ccc",
+        ],
+      },
+      layout: {
+        visibility: "none",
+      },
+    });
 
   //Layer Zonasi
   map.addSource("zonasi", {
@@ -71,6 +154,15 @@ const onOffLayer = () => {
       hideLayer("wilayah_fill");
     }
   });
+  $("#persil_fill").change(() => {
+    if ($("#persil_fill").is(":checked")) {
+      showLayer("persil-fill");
+      showLayer("persil-border");
+    } else {
+      hideLayer("persil-fill");
+      hideLayer("persil-border");
+    }
+  });
   $("#zonasi_fill").change(() => {
     if ($("#zonasi_fill").is(":checked")) {
       showLayer("zonasi_fill");
@@ -85,6 +177,18 @@ onOffLayer();
 map.addControl(new mapboxgl.NavigationControl());
 
 map.addControl(draw);
+
+map.on("mouseenter", "polygons-fill", (e) => {
+  // console.log(e);
+  map.getCanvas().style.cursor = "pointer";
+});
+map.on("mouseleave", "polygons-fill", (e) => {
+  // console.log(e);
+  map.getCanvas().style.cursor = "";
+});
+map.on("click", "polygons-fill", (e) => {
+  console.log(e.features[0].properties);
+});
 
 $(".mapboxgl-ctrl.mapboxgl-ctrl-attrib, a.mapboxgl-ctrl-logo").css(
   "visibility",
